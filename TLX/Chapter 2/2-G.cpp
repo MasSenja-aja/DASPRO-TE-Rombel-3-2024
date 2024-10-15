467560d64119d71943da922d96d89a998696a398
MasSenja-aja <andawaras1140@gmail.com>
2024-10-15 13:34:37 +0700

    Create 2-G.cpp

---
+++ TLX/Chapter 2/2-G.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int jantan, betina;
+	
+	// banyakmya bebek saat ini
+	jantan = 63;
+	betina = 192;
+	
+	// setelah bulan pertama
+	betina = 192 + 63;
+	jantan = 63 - (63 /3);
+	
+	// setelah bulan kedua 
+	jantan = 42 + 255;
+	betina = 255 - 10;
+	
+	// cetak total bebek
+	cout << 297 + 245 << endl;
+}
