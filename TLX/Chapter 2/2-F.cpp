ec4c7611c19db1c64a40de57ca5d656efc4772e6
MasSenja-aja <andawaras1140@gmail.com>
2024-10-15 13:37:55 +0700

    Create 2-F.cpp

---
+++ TLX/Chapter 2/2-F.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int sandi;
+	int tanggal = 15, tahun = 2023;
+	
+	// banyaknya bebek di kandang
+	int total_bebek = 174;
+	
+	// sandi bulan pertama adalah banyaknya bebek
+	sandi = total_bebek;
+	cout << sandi << endl;
+	
+}
